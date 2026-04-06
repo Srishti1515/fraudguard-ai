90a31b596796cf7cd4c479699c9c577effd6ee52
Srishti1515 <goelsrishti.22@gmail.com>
2026-04-07 01:20:42 +0530

    Fix API URL (remove localhost)

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,19 +1,8 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL + "/api",
   timeout: 15000,
 });
 
-api.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    if (err.response?.status === 401) {
-      localStorage.removeItem('fraudguard-auth')
-      window.location.href = '/login'
-    }
-    return Promise.reject(err)
-  }
-)
-
-export default api
+export default api;
